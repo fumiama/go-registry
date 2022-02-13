0bb4b0bd81f23e3e1ae08a01e68c28e2b1c7beb1
fumiama <s862105088@gmail.com>
2022-02-13 12:14:06 +0800

    add push

--- reg_test.go
+++ reg_test.go
@@ -45,93 +45,11 @@ func TestPush(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	/*
-		err = r.Set("ZeroBot-Plugin/kanban", "QQ群:1048452984, 开发群:705749886. 禁止用于商业用途.\nv1.2.2及以下版本公告服已下线.")
-		if err != nil {
-			t.Fatal(err)
-		}
-		m, err := hex.DecodeString("48f7021b54af01360a99e0f0e4937bc2")
-		if err != nil {
-			t.Fatal(err)
-		}
-		err = r.Set("data/BookReview/bookreview.db", helper.BytesToString(m))
-		if err != nil {
-			t.Fatal(err)
-		}
-		m, err = hex.DecodeString("ae4448ea660c10ffa025bab643566c04")
-		if err != nil {
-			t.Fatal(err)
-		}
-		err = r.Set("data/Diana/text.pb", helper.BytesToString(m))
-		if err != nil {
-			t.Fatal(err)
-		}
-		m, err = hex.DecodeString("f4d6b91f203c89eb42b0a92518a29502")
-		if err != nil {
-			t.Fatal(err)
-		}
-		err = r.Set("data/Diana/text.db", helper.BytesToString(m))
-		if err != nil {
-			t.Fatal(err)
-		}
-		m, err = hex.DecodeString("5dde28c2d5c55cc2fd2e9f8276786606")
-		if err != nil {
-			t.Fatal(err)
-		}
-		err = r.Set("data/Omikuji/kuji.db", helper.BytesToString(m))
-		if err != nil {
-			t.Fatal(err)
-		}
-		m, err = hex.DecodeString("86d9be1db96a74094f9660d47d8d3af7")
-		if err != nil {
-			t.Fatal(err)
-		}
-		err = r.Set("data/Reborn/rate.json", helper.BytesToString(m))
-		if err != nil {
-			t.Fatal(err)
-		}
-		m, err = hex.DecodeString("9ed2e3922ffe50fda24a566fbd152f7b")
-		if err != nil {
-			t.Fatal(err)
-		}
-		err = r.Set("data/SetuTime/SetuTime.db", helper.BytesToString(m))
-		if err != nil {
-			t.Fatal(err)
-		}
-		m, err = hex.DecodeString("a02630a6811e93c398a7361e04c1c865")
-		if err != nil {
-			t.Fatal(err)
-		}
-		err = r.Set("data/VtbQuotation/vtb.db", helper.BytesToString(m))
-		if err != nil {
-			t.Fatal(err)
-		}
-		m, err = hex.DecodeString("471cebf6b8509abd4c9169e71dff7618")
-		if err != nil {
-			t.Fatal(err)
-		}
-		err = r.Set("data/Chat/kimoi.json", helper.BytesToString(m))
-		if err != nil {
-			t.Fatal(err)
-		}
-		m, err = hex.DecodeString("3db5174e9625ec85ba3a0d48a21eb080")
-		if err != nil {
-			t.Fatal(err)
-		}
-		err = r.Set("data/Funny/jokes.db", helper.BytesToString(m))
-		if err != nil {
-			t.Fatal(err)
-		}
-		m, err := hex.DecodeString("ef27cbb457d018423e9b6af5edca7a21")
-		if err != nil {
-			t.Fatal(err)
-		}
-		err = r.Set("data/ChouXiangHua/cxh.db", helper.BytesToString(m))
-		if err != nil {
-			t.Fatal(err)
-		}
-		err = r.Close()
-	*/
+	err = r.Set("ZeroBot-Plugin/kanban", "QQ群:1048452984, 开发群:705749886. 禁止用于商业用途.\nv1.2.2及以下版本公告服已下线.")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = r.Close()
 	if err != nil {
 		t.Fatal(err)
 	}
